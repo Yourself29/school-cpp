893d0acb807976164e7de2fc30dec1cadc0dfb88
TarunKumar29 <66009888+TarunKumar29@users.noreply.github.com>
2021-02-03 21:22:15 +0530

    Create program-2.cpp

---
+++ assignment-1/program-2.cpp
@@ -0,0 +1,55 @@
+# include <iostream> 
+
+# include <cmath> 
+
+#include <stdlib.h> 
+
+ 
+ 
+
+using namespace std; 
+
+ 
+ 
+
+int main(void) 
+
+{ 
+
+ 
+ 
+
+system("CLEAR"); 
+
+ 
+ 
+
+double i; 
+
+ 
+ 
+
+cout << "Enter a number: "; 
+
+cin >> i; 
+
+ 
+ 
+
+system("CLEAR"); 
+
+ 
+ 
+
+cout << "Number: " << i << '\n'; 
+
+cout << "Square: " << pow(i, 2) << '\n'; 
+
+cout << "Cube: " << pow(i, 3) << '\n'; 
+
+ 
+ 
+
+return 0; 
+
+} 
