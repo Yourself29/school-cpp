0a2eb53f715ab0f5dee53ed180823248bf1b481d
TarunKumar29 <66009888+TarunKumar29@users.noreply.github.com>
2021-02-05 22:28:45 +0530

    Update program-4.cpp

--- assignment-1/program-4.cpp
+++ assignment-1/program-4.cpp
@@ -1,73 +1,32 @@
-# include <iostream> 
+# include <iostream>
+# include <math.h>
+#include <stdlib.h>
 
-# include <math.h> 
+using namespace std;
 
-#include <stdlib.h> 
+int main(void)
+{
 
- 
- 
+system("CLEAR");
 
-using namespace std; 
+double num1, num2;
 
- 
- 
+cout << "Enter the first number: ";
+cin >> num1;
 
-int main(void) 
+system("CLEAR");
 
-{ 
+cout << "Enter the second number: ";
+cin >> num2;
 
- 
- 
+system("CLEAR");
 
-system("CLEAR"); 
+cout << "Numbers: " << num1 << ", " << num2 << '\n';
+cout << "Sum: " << (num1+num2) << '\n';
+cout << "Difference: " << (num1-num2) << '\n';
+cout << "Product: " << (num1*num2) << '\n';
+cout << "Quotient: " << (num1/num2) << '\n';
+cout << "Remainder: " << fmod(num1, num2) << '\n';
 
- 
- 
-
-double num1, num2; 
-
- 
- 
-
-cout << "Enter the first number: "; 
-
-cin >> num1; 
-
- 
- 
-
-system("CLEAR"); 
-
- 
- 
-
-cout << "Enter the second number: "; 
-
-cin >> num2; 
-
- 
- 
-
-system("CLEAR"); 
-
- 
- 
-
-cout << "Numbers: " << num1 << ", " << num2 << '\n'; 
-
-cout << "Sum: " << (num1+num2) << '\n'; 
-
-cout << "Difference: " << (num1-num2) << '\n'; 
-
-cout << "Product: " << (num1*num2) << '\n'; 
-
-cout << "Quotient: " << (num1/num2) << '\n'; 
-
-cout << "Remainder: " << fmod(num1, num2) << '\n'; 
-
- 
- 
-
-return 0; 
-
-} 
+return 0;
+}
