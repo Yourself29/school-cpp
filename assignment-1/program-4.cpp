4f5355e271d9ecc5190136b17ff0f6b4439ef904
TarunKumar29 <66009888+TarunKumar29@users.noreply.github.com>
2021-02-05 22:26:10 +0530

    Create program-4.cpp

---
+++ assignment-1/program-4.cpp
@@ -0,0 +1,73 @@
+# include <iostream> 
+
+# include <math.h> 
+
+#include <stdlib.h> 
+
+ 
+ 
+
+using namespace std; 
+
+ 
+ 
+
+int main(void) 
+
+{ 
+
+ 
+ 
+
+system("CLEAR"); 
+
+ 
+ 
+
+double num1, num2; 
+
+ 
+ 
+
+cout << "Enter the first number: "; 
+
+cin >> num1; 
+
+ 
+ 
+
+system("CLEAR"); 
+
+ 
+ 
+
+cout << "Enter the second number: "; 
+
+cin >> num2; 
+
+ 
+ 
+
+system("CLEAR"); 
+
+ 
+ 
+
+cout << "Numbers: " << num1 << ", " << num2 << '\n'; 
+
+cout << "Sum: " << (num1+num2) << '\n'; 
+
+cout << "Difference: " << (num1-num2) << '\n'; 
+
+cout << "Product: " << (num1*num2) << '\n'; 
+
+cout << "Quotient: " << (num1/num2) << '\n'; 
+
+cout << "Remainder: " << fmod(num1, num2) << '\n'; 
+
+ 
+ 
+
+return 0; 
+
+} 
